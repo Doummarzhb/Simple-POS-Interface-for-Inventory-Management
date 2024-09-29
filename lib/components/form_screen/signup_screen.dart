25c1ed9effc307985bd68e800eb1017074fb382d
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2024-09-29 18:29:14 +0300

    first commit

--- lib/components/form_screen/signup_screen.dart
+++ lib/components/form_screen/signup_screen.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:management/screens/home_screen.dart';
-import 'package:management/components/form_screen/signup_screen.dart';     
 
-class FormScreen extends StatefulWidget {
+class SignUpScreen extends StatefulWidget {
   @override
-  State<FormScreen> createState() => _FormScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _FormScreenState extends State<FormScreen> {
-  final String staticEmail = "user@gmail.com";
-  final String staticPassword = "password123";
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Simple POS Interface for Inventory Management | Clothing Store", style: TextStyle(color: Colors.white),),
+        title: Text("Sign Up", style: TextStyle(color: Colors.white),),
         centerTitle: true,
-        backgroundColor: Colors.black, 
+        backgroundColor: Colors.black,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -30,42 +31,52 @@ class _FormScreenState extends State<FormScreen> {
                 Image.asset("images/avatar.png", height: 150, width: 150,),
                 SizedBox(height: 30),
                 TextFormField(
-                  initialValue: staticEmail,  
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: "Name",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: emailController,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     labelText: "Email",
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.email),
                   ),
-                  enabled: false,    
                 ),
                 SizedBox(height: 20),
                 TextFormField(
-                  initialValue: staticPassword,       
+                  controller: passwordController,
                   obscureText: true,
                   decoration: InputDecoration(
                     labelText: "Password",
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.lock),
                   ),
-                  enabled: false,           
                 ),
                 SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text("Forgot Password?", style: TextStyle(color: Color(0xFF295F98),)),
+                TextFormField(
+                  controller: confirmPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
                   ),
                 ),
+                SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) => HomeScreen(
-                          email: staticEmail,
-                          password: staticPassword,           
+                          email: emailController.text,
+                          password: passwordController.text,
                         ),
                       ),
                     );
@@ -74,7 +85,7 @@ class _FormScreenState extends State<FormScreen> {
                     primary: Colors.black,
                     padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                   ),
-                  child: Text("Login", style: TextStyle(color: Colors.white),),
+                  child: Text("Sign Up", style: TextStyle(color: Colors.white),),
                 ),
                 SizedBox(height: 20),
                 Row(
@@ -83,15 +94,10 @@ class _FormScreenState extends State<FormScreen> {
                     Text("Already have an account?"),
                     TextButton(
                       onPressed: () {
-                      
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SignUpScreen(),  
-                          ),
-                        );
+                        
+                        Navigator.pop(context);  
                       },
-                      child: Text("Sign Up", style: TextStyle(color: Color(0xFF295F98),)),
+                      child: Text("Login", style: TextStyle(color: Color(0xFF295F98),)),
                     ),
                   ],
                 ),
